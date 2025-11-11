fe6f759eb6e81f295bf159f8f3107644cc0cad3e
james <32554945+jmichaelov@users.noreply.github.com>
2025-11-11 14:23:16 -0500

    fix socrates plus linting

--- lm_eval/tasks/socrates/utils.py
+++ lm_eval/tasks/socrates/utils.py
@@ -1,9 +1,24 @@
 import re
 
 
-def exact_match(references, predictions):
-    pred = predictions[0]
-    ref = references[0]
+def exact_match_e2(docs, predictions):
+    try:
+        pred = predictions[0]
+    except IndexError:
+        print(str(predictions))
+    ref = docs["e2_aliases"]
+
+    match = float(int(bool(re.search(r"|".join(ref), pred, re.IGNORECASE))))
+
+    return {"exact_match": match}
+
+
+def exact_match_e3(docs, predictions):
+    try:
+        pred = predictions[0]
+    except IndexError:
+        print(str(predictions))
+    ref = docs["e3_aliases"]
 
     match = float(int(bool(re.search(r"|".join(ref), pred, re.IGNORECASE))))
 
