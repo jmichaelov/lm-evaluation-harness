fdf7173226a7932256aad7077f78072c03843194
james <32554945+jmichaelov@users.noreply.github.com>
2025-11-11 15:09:54 -0500

    redo

--- lm_eval/tasks/socrates/utils.py
+++ lm_eval/tasks/socrates/utils.py
@@ -1,7 +1,7 @@
 import re
 
 
-def exact_match_e2(references, predictions):
+def contains_correct(references, predictions):
     try:
         pred = predictions[0]
     except IndexError:
@@ -9,21 +9,7 @@ def exact_match_e2(references, predictions):
     ref = references
     for ref in references:
         if ref.lower() in pred.lower():
-            return {"exact_match_e2": 1.0}
+            return {"contains_correct": 1.0}
     # match = float(int(bool(re.search(r"("+r")|(".join(ref)+r")", pred, re.IGNORECASE))))
 
-    return {"exact_match_e2": 0.0}
-
-
-def exact_match_e3(references, predictions):
-    try:
-        pred = predictions[0]
-    except IndexError:
-        print(str(predictions))
-    ref = references
-    for ref in references:
-        if ref.lower() in pred.lower():
-            return {"exact_match_e3": 1.0}
-    # match = float(int(bool(re.search(r"("+r")|(".join(ref)+r")", pred, re.IGNORECASE))))
-
-    return {"exact_match_e3": 0.0}
+    return {"contains_correct": 0.0}
