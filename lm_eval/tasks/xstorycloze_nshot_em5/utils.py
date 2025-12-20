8dfd41fa456c7175b317c43b1629d741d5a323d0
james <32554945+jmichaelov@users.noreply.github.com>
2025-12-20 10:41:45 -0500

    em5

---
+++ lm_eval/tasks/xstorycloze_nshot_em5/utils.py
@@ -0,0 +1,33 @@
+def doc_to_text(doc):
+    text = (
+        (
+            doc["input_sentence_1"]
+            + " "
+            + doc["input_sentence_2"]
+            + " "
+            + doc["input_sentence_3"]
+            + " "
+            + doc["input_sentence_4"]
+            + " "
+            + [doc["sentence_quiz1"], doc["sentence_quiz2"]][
+                int(doc["answer_right_ending"]) - 1
+            ]
+            + " "
+        )
+        * 4
+        + doc["input_sentence_1"]
+        + " "
+        + doc["input_sentence_2"]
+        + " "
+        + doc["input_sentence_3"]
+        + " "
+        + doc["input_sentence_4"]
+    )
+
+    return text
+
+
+def get_target(doc):
+    return [doc["sentence_quiz1"], doc["sentence_quiz2"]][
+        int(doc["answer_right_ending"]) - 1
+    ]
